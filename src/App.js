e3d0563f0bd053d62fe18895c59196edd150d5c3
DelphicGen <50311626+DelphicGen@users.noreply.github.com>
2020-09-05 14:22:19 +0700

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [username, setUsername] = useState("instagram");
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <img src="https://via.placeholder.com/300" alt="prfile picture" />
+      <div>@{username}</div>
+      <button>Follow</button>
+      <div>Posts</div>
+      <div>Followers</div>
+      <div>Following</div>
+      <div>Bio</div>
+    </>
   );
 }
